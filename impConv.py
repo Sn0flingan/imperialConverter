3cea3bdbd56c2daffd79ff47e92ef71e55884d24
Alice Anlind <alice.anlind@gmail.com>
2017-07-07 01:07:43 +0200

    Create impConv.py

---
+++ impConv.py
@@ -0,0 +1,7 @@
+# General idea
+# Take in unit of lengt as argument to script
+# Convert to m
+# Find imperial units that are between 1-10
+# Rank according to lowest modulus
+
+#Read input and convert to correct method
